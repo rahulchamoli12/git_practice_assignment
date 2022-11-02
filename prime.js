4dbabbf33796b96e2ad7c0d9f5a83b145102ed62
Rahul Chamoli <rahulchamoli518@gmail.com>
2022-11-02 12:52:51 +0530

    Solved by me

--- prime.js
+++ prime.js
@@ -1,8 +1,8 @@
 function prime(number){
 	let factor=0;
-for(let i=0; i<=number; i++){
-	if(number%i){
-	fac++;
+for(let i=0; i<=num; i++){
+	if(number%i==0){
+	factor++;
 	}
 	}
 if(factor == 2){
@@ -13,7 +13,7 @@ return false;
 }
 
 let answer = prime(13);
-if(answer==true){
+if(ans==true){
 console.log("Is a Prime Number");
 }else{
 console.log("Is Not a Prime Number");
